c74a6226d082f36fc8dddad1b1023d8f07a3ed28
Oskar Fagerfjäll <oskar.fagerfjall@gmail.com>
2020-12-11 17:13:44 +0900

    Solve day 10 part 1

--- 2020/10/solution.py
+++ 2020/10/solution.py
@@ -1,1 +1,19 @@
 # Seems like it should be enough to sort an array and then step through it.
+
+def count_adapters( s ):
+    adapters = list( map(int, s.split()))
+    adapters.append( max( adapters ) + 3 )
+    adapters = sorted( adapters )
+    jolt = 0
+    counts = [0,0,0]
+    for a in adapters:
+        diff = a - jolt
+        if diff > 3:
+            raise "NoFittingAdapter"
+        elif diff > 0:
+            jolt += diff
+            counts[diff-1] += 1
+        else:
+            pass
+    return counts
+
--- 2020/10/test.py
+++ 2020/10/test.py
@@ -1,3 +1,5 @@
+from solution import count_adapters
+
 a = """16
 10
 15
@@ -11,9 +13,10 @@ a = """16
 4
 """
 
-diffa = [7, 0, 5]
+result = count_adapters( a )
+expected = [7, 0, 5]
 
-
+print( str(result) + " == " + str(expected))
 
 b = """28
 33
@@ -48,4 +51,12 @@ b = """28
 3
 """
 
-diffb = [22, 0, 10]
+result = count_adapters( b )
+expected = [22, 0, 10]
+
+print( str(result) + " == " + str(expected))
+
+with open("2020/10/input.txt", "r") as infile:
+    result = count_adapters( infile.read() )
+    print( str(result) )
+    print( str( result[0] * result [2] ))
